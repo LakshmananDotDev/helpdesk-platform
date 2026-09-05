057e758820baecf1c15c979f530c418927d44bef
Sachin <lakshmananvs1212@gmail.com>
2026-09-05 11:49:36 +0530

    Module 0: initial Spring Boot project skeleton

---
+++ src/main/java/com/helpdesk/helpdeskplatform/HelpdeskPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.helpdesk.helpdeskplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpdeskPlatformApplication.class, args);
+    }
+
+}
